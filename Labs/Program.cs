0f165a44f21a8f0cc54e007aadf6c0fba97f69cf
Daria45387354978 <dbatalova57@gmail.com>
2025-04-06 20:51:29 +0500

    цув
    цвув

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("Введите координаты точки (x и y):");
+                double x = Convert.ToDouble(Console.ReadLine());
+                double y = Convert.ToDouble(Console.ReadLine());
+
+                if (x == 0 && y == 0)
+                {
+                    Console.WriteLine("Точка находится в начале координат.");
+                }
+                else if (x == 0)
+                {
+                    Console.WriteLine("Точка находится на оси Y.");
+                }
+                else if (y == 0)
+                {
+                    Console.WriteLine("Точка находится на оси X.");
+                }
+                else if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("Точка находится в первом квадранте.");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("Точка находится во втором квадранте.");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("Точка находится в третьем квадранте.");
+                }
+                else
+                {
+                    Console.WriteLine("Точка находится в четвертом квадранте.");
+                }
+            }
+        }
+
+    }
+
+

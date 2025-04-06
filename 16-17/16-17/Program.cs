7c6218ae7e843330303e7b28267172d5362a0fc1
Daria45387354978 <dbatalova57@gmail.com>
2025-04-06 20:55:48 +0500

    фцв
    фв

---
+++ 16-17/16-17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите натуральное число n: ");
+        string input = Console.ReadLine();
+
+
+        if (int.TryParse(input, out int n) && n > 0)
+        {
+            int sum = 0;
+
+
+            while (n > 0)
+            {
+                sum += n % 10;
+                n /= 10;
+            }
+
+            Console.WriteLine($"Сумма цифр числа: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введите корректное натуральное число.");
+        }
+    }
+}
